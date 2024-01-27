486ddb93f5729795affbc947874397b36f45ab50
shaoqing.wu <shaoqing_wu@foxmail.com>
2024-01-27 22:18:46 +0800

    v1 test pass

--- projects/configs/fastbev/fastbev_v1_occ.py
+++ projects/configs/fastbev/fastbev_v1_occ.py
@@ -137,7 +137,7 @@ test_pipeline = [
 
 data = dict(
     samples_per_gpu=2,
-    workers_per_gpu=4,
+    workers_per_gpu=6,
     train=dict(
         type=dataset_type,
         data_root=data_root,
